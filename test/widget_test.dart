cf45e83a4181d689695c0d4058ab780655f893ec
Andezion <245122@edu.p.lodz.pl>
2026-04-17 14:34:16 +0200

    add: fixed tests naming

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:citygame/main.dart';
+import 'package:CityGame/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
